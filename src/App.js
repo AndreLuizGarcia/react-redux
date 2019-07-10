a99609f7134eb3fad26396414671f0358a131c91
Andre Garcia <Andre.Garcia@la.logicalis.com>
2019-07-10 19:09:35 -0300

    Cleaned Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux'  //Responsavel por atualizar os componentes da aplicação quando houver alteração no estado da aplicação.
+import store from './store'
+import TodoList from './TodoList'
+import Counter from './Counter'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div>
+          <TodoList />
+          <Counter />
+        </div>
+      </Provider>
+
+    )
+  }
 }
 
-export default App;
+export default App
